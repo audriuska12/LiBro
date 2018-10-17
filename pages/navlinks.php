b9ae07ebe5843ab4075434910ba9803da4bb66f4
Audrius Frankonis <audrius.frankonis@ktu.edu>
2018-10-17 22:43:39 +0300

    Hamburger menu done; TODO - responsive layout for authors/series, more detail in H/F, transitions/animations; vector icons; modal window.

--- pages/navlinks.php
+++ pages/navlinks.php
@@ -1,4 +1,13 @@
-<div style="font-size:2em;">
+<div class="dropdown" id="navlinksSmallscreen">
+    <button class="dropbtn">Links</button>
+    <div class="dropdown-content">
+      <div><a href="http://localhost/LiBro/books">Books</a></div>
+      <div><a href="http://localhost/LiBro/authors">Authors</a></div>
+      <div><a href="http://localhost/LiBro/series">Series</a></div>
+    </div>
+ </div>
+
+<div id="navlinksWidescreen">
 	<a href="http://localhost/LiBro/books">Books</a>
 	<a href="http://localhost/LiBro/authors">Authors</a>
 	<a href="http://localhost/LiBro/series">Series</a>
