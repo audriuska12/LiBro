7ed1b4b194d740ec090fd6e31e2980fd13e2c2e1
Audrius Frankonis <audrius.frankonis@ktu.edu>
2018-10-25 22:32:32 +0300

    SVG added (not in repo due to copyright), footer updated - COMPLETE!

--- pages/footer.php
+++ pages/footer.php
@@ -1,1 +1,10 @@
-<div id="footer">LiBro Footer</div>
+<div id="footer">
+	<div id="footer-content">
+		<img id="footer-img" src="http://localhost/LiBro/images/book.svg"></img>
+		<div id="footer-links">
+			<a href="http://localhost/LiBro/books/">Books</a>|
+			<a href="http://localhost/LiBro/authors/">Authors</a>|
+			<a href="http://localhost/LiBro/series/">Series</a>
+		</div>
+	</div>
+</div>
